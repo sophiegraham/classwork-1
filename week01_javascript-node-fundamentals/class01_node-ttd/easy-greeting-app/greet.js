8412cca933189bb1291b82da934066a65f02599c
martypdx <marty@martypdx.com>
2018-09-24 13:11:15 -0700

    delinate three jobs of simple cli file

--- week01_javascript-node-fundamentals/class01_node-ttd/easy-greeting-app/greet.js
+++ week01_javascript-node-fundamentals/class01_node-ttd/easy-greeting-app/greet.js
@@ -1,3 +1,16 @@
 /* eslint no-console: off */
 
-console.log('hello ' + process.argv[2]);
+// original:
+// console.log('hello ' + process.argv[2]);
+
+// break down into "jobs":
+
+// #1 process arguments
+// get the name to greet
+const name = process.argv[2];
+
+// #2 format greeting
+const greeting = 'hello ' + name;
+
+// #3 Display greeting on CLI
+console.log(greeting);
