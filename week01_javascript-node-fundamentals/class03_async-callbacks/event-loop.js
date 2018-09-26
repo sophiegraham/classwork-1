7cd683c9d55f94dececd065f1c4baf0a0a862c26
Ryan Mehta <ryan@vrtigo.io>
2018-09-26 09:59:24 -0700

    event loop

---
+++ week01_javascript-node-fundamentals/class03_async-callbacks/event-loop.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const http = require('http');
+const request = require('superagent');
+
+console.log('A - Start of file');
+
+fs.readFileSync('foo.txt');
+
+console.log('A.1/2 - after readFileSync');
+
+[1, 4, 6].forEach(n => console.log(n));
+
+fs.readFile('~/big.pdf', 'utf8', (err, data) => {
+    console.log('B - big.pdf callback');
+});
+
+fs.readFile('./event-loop.js', 'utf8', (err, data) => {
+    console.log('C - event-loop.js callback');
+});
+
+http.createServer((req, res) => {
+    console.log('D - web server callback');
+    res.end();
+}).listen(8080);
+
+setTimeout(() => {
+    console.log('E - setTimeout 1000 callback');
+}, 1000);
+
+request.get('www.google.com').end((err, res) => {
+    console.log('F - get google callback');
+});
+
+console.log('G - End of file');
