d4e265619416ad40ff34bdb622d5ab102d33c306
martypdx <marty@martypdx.com>
2018-10-01 11:06:38 -0700

    remove preflight

--- week02_node-server-fundamentals/class06_tcp-servers/_preflight/configured-server-start.js
+++
@@ -1,8 +0,0 @@
-const makeLogServer = require('./configured-server');
-
-const PATH = './log.txt';
-const PORT = 15666;
-
-const server = makeLogServer(PATH);
-
-server.listen(PORT);
--- week02_node-server-fundamentals/class06_tcp-servers/_preflight/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./lib/app');
-
-const PORT = 15678;
-
-app.on('listening', () => {
-    console.log('TCP Server listening on port', PORT);
-});
-
-app.listen(PORT);
