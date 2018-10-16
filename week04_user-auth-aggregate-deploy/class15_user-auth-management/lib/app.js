5085507913f7bca52b566996da96f547f4d25a08
Ryan Mehta <ryan@vrtigo.io>
2018-10-16 14:28:02 -0700

    add app js

---
+++ week04_user-auth-aggregate-deploy/class15_user-auth-management/lib/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const { handler } = require('./util/errors');
+
+app.use(morgan('dev', {
+    skip() {
+        // skip logging on test
+        return process.env.NODE_ENV === 'test';
+    }
+}));
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.use((req, res) => {
+    console.log('This is 404');
+    res.status(404);
+    res.end('404 Not Found');
+});
+
+// Error Handler middleware is last!
+app.use(handler);
+
+module.exports = app;
